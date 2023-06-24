a02a49f0bbf824225e7faa52c60bb31f24d946f0
DenysPetroff <petroffich2008@gmail.com>
2023-06-25 01:08:28 +0300

    hw-js-7-fin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');
+
+list.insertAdjacentHTML('beforeend', markup);
+
+list.addEventListener('click', handlerClick);
+let originalImg;
+function handlerClick(evt) {
+    evt.preventDefault();
+    // originalImg = evt.target.closest('.gallery__link').href;
+    originalImg = evt.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+    <img src="${originalImg}" width="800" height="600">`)
+
+    instance.show();
+    
+    if (instance.show()) {
+
+    document.addEventListener('keydown', handlerEsc);
+    
+    function handlerEsc(evt) {
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+    }
+};
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join('');
+
+list.insertAdjacentHTML('beforeend', markup);
+// console.log(SimpleLightbox);
+
+list.addEventListener('click', handlerClick);
+
+function handlerClick(evt) {
+    if (evt.target.tagName === 'IMG') {
+        evt.preventDefault();
+        const gallery = new SimpleLightbox('.gallery a', { captionsData: `alt`, captionDelay: 250, });
+        gallery.open();
+        }
+        
+}
